746825baf7dc253937225cd10d66899852de4c82
Darma <darmawanruslan12@gmail.com>
2024-04-25 17:34:07 +0700

    Initial Commit

---
+++ GestureTest/GestureTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GestureTestApp.swift
+//  GestureTest
+//
+//  Created by Dharmawan Ruslan on 25/04/24.
+//
+
+import SwiftUI
+
+@main
+struct GestureTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
